b9590920f49b5a25c7f1b635374edb5c2e8b235d
Ashley Bao <abao2003@gmail.com>
2022-11-11 18:03:02 -0500

    created main and language files
    - started commenting in the structure of the Language class

---
+++ .idea/src/Language.java
@@ -0,0 +1,53 @@
+
+import java.util.*;
+import java.io.*;
+public class Language {
+    //fields
+
+    int[] countLetters;
+    //countLetters[0] = number of a's in the text
+    int[][] countPairs;
+    //countPairs[0][0] = number of "aa"'s in the text
+    int[][] pairProbability;
+    //pairProbability[0][0] = probability of "aa" in the text
+    String languageName;
+
+    public Language ()
+    {
+
+    } //constructor: need to initialize the arrays
+
+    /*
+    public int[][] loadLanguage(String name, File f)
+    {
+        reads the file and returns 2d array of frequencies
+    }
+    */
+
+    /*
+    public double probabilityOf(String sentence)
+    {
+         prob[first letter of sentence][second letter]*prob[second letter][third letter]* ...
+
+    }
+    */
+
+    /*
+    public int countLetters(char c)
+    {
+
+    }
+    */
+
+
+    /*
+    public int countLetterPair(char a, char b)
+    {
+
+    }
+     */
+
+
+
+
+}
